1dd67f3f0cc354ad1788e3ac050711b88eaa14a1
Realxeng <realxengpandelaki@gmail.com>
2025-05-23 18:28:12 +0800

    Created base UI for login page

--- nab/lib/login.dart
+++ nab/lib/login.dart
@@ -5,23 +5,34 @@ import 'package:nab/customer/cus_home_page.dart';
 class Login extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final screenWidth = MediaQuery.of(context).size.width;
     return Scaffold(
-      appBar: AppBar(title: Text('NAB Car Rental')),
       body: Center(
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            Text('Login to your Nab account', style: TextStyle(fontSize: 20)),
+            SizedBox(height: 100),
+            Image.asset('assets/Nab_Emblem.png', width: 240, height: 150),
+            Text(
+              'Login to your account',
+              style: TextStyle(fontSize: 20, fontFamily: 'Arial'),
+            ),
             SizedBox(height: 20),
-            TextField(decoration: InputDecoration(labelText: 'Email')),
-            TextField(
-              decoration: InputDecoration(labelText: 'Password'),
-              obscureText: true,
+            Container(
+              width: screenWidth * 0.8, // 80% of the screen width
+              child: TextField(decoration: InputDecoration(labelText: 'Email')),
+            ),
+            SizedBox(height: 16), // Add spacing between fields
+            Container(
+              width: screenWidth * 0.8, // 80% of the screen width
+              child: TextField(
+                decoration: InputDecoration(labelText: 'Password'),
+                obscureText: true,
+              ),
             ),
             SizedBox(height: 20),
             ElevatedButton(
               onPressed: () {
-                // Add login functionality here
+                checkUser();
               },
               child: Text('Login'),
             ),
@@ -31,3 +42,5 @@ class Login extends StatelessWidget {
     );
   }
 }
+
+void checkUser() {}
