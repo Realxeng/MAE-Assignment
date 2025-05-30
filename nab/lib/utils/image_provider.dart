fc798f6e8a062e14826f1955951bcafd40e49865
stanleywijayaa <stanleytan2018@gmail.com>
2025-05-30 19:44:05 +0800

    modified the base64 converter & decoder

--- nab/lib/utils/image_provider.dart
+++ nab/lib/utils/image_provider.dart
@@ -1,19 +1,23 @@
 import 'dart:convert';
 import 'dart:io';
+import 'dart:typed_data';
 
 class ImageConstants {
   static final ImageConstants constants = ImageConstants._();
   factory ImageConstants() => constants;
   ImageConstants._();
-  dynamic convertToBase64(File file) {
+
+  String convertToBase64(File file) {
     List<int> imageBytes = file.readAsBytesSync();
     String base64Image = base64Encode(imageBytes);
-
     return 'data:image/jpeg;base64,$base64Image';
   }
 
-  dynamic decodeBase64(String encoded) {
-    String decoded = utf8.decode(base64Url.decode(encoded));
-    return decoded;
+  /// Decodes base64 string (with or without data:image/... prefix) to bytes
+  Uint8List decodeBase64(String base64String) {
+    // Remove prefix if exists (data:image/jpeg;base64,)
+    final regex = RegExp(r'data:image/[^;]+;base64,');
+    final pureBase64 = base64String.replaceAll(regex, '');
+    return base64Decode(pureBase64);
   }
 }
